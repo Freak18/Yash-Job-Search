b798ff308e1ffe82e539fe45dd140d64840da817
Yashwanth Varma <k.yashwanthvarma@gmail.com>
2026-06-06 23:54:13 +0530

    Fix Vercel configuration

--- api/index.py
+++ api/index.py
@@ -2,7 +2,10 @@ import sys
 from pathlib import Path
 
 ROOT = Path(__file__).resolve().parent.parent
+
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
 
 from server import app
+
+app = app
